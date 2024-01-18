dd781d613d845e4a20e4f61d640046230a1938b4
EggsyJ <fordev0905@gmail.com>
2024-01-18 22:00:50 +0900

    create boilerplate

---
+++ user-order/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Container">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
